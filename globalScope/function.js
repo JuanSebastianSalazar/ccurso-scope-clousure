5b9cdcb87abc8f8483a0938dfc1e5708ec15eb3d
Sebastian <juansebasalazar019@gmail.com>
2023-12-17 18:33:39 -0500

    video 1 - 3

---
+++ globalScope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let userName = 'Ana';
+    console.log(userName);
+    
+    if(userName === 'Ana'){
+      console.log( `Hello  ${UserName}!`)
+    
+       }
+    }
+    
+    greeting();
+    console.log(userName);// no esta asignada como global por eso no se muestra
---
+++ globalScope/global.js
@@ -0,0 +1,23 @@
+let a; //declarando
+let b = 'b'; //declaramos y asignamos
+b = 'bb'; //reasignacion
+
+
+//GLOBAL SCOPE  
+
+let fruit = 'Apple'; //global
+
+function bestFruit() {
+    console.log(fruit)
+}
+
+bestFruit();
+
+
+
+function countries() {
+    country = 'Colombia'; //
+    console.log(country);
+}
+
+countries();
